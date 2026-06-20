5c6dedd03ae66c89f78c8b4f3c18afeab326d43d
Davi Motta <davimotta2003@hotmail.com>
2026-06-20 14:10:21 -0300

    Adicionando exemplo do uso de variáveis

---
+++ Fundamentos/Exemplos/variavel.py
@@ -0,0 +1,16 @@
+nome = "Davi"
+idade = 23
+altura = 1.75
+estudante = True
+
+lista_compras = ["leite", "pão", "ovos"]
+dicionario_pessoa = {"nome": "Davi", "idade": 23, "altura": 1.75}
+tupla_cores = ("vermelho", "verde", "azul")
+conjunto_numeros = {1, 2, 3, 4, 5}
+
+print(f"Olá, me chamo {nome}, tenho {idade} anos e {altura}m de altura. Esse é nosso exemplo com variáveis simples!")
+
+print(f"Nossa lista de compras é: {lista_compras}")
+print(f"Um exemplo com dicionário com as informações que temos: {dicionario_pessoa}")
+print(f"Uma tupla de cores para exemplificar sua utilização: {tupla_cores}")
+print(f"Um exemplo de conjunto de números: {conjunto_numeros}")
