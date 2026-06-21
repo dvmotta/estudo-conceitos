8e243a7c46590ce03a8b1f62a6daeaf1010fe340
Davi Motta <davimotta2003@hotmail.com>
2026-06-20 22:49:27 -0300

    Improve repository structure and translate content

---
+++ Foundation/Variables and Types/main.py
@@ -0,0 +1,16 @@
+name = "Davi"
+age = 23
+height = 1.75
+student = True
+
+shopping_list = ["milk", "bread", "eggs"]
+person_dict = {"name": "Davi", "age": 23, "height": 1.75}
+color_tuple = ("red", "green", "blue")
+set_numbers = {1, 2, 3, 4, 5}
+
+print(f"Hello, my name is {name}, I am {age} years old and {height}m tall. This is our example with simple variables!")
+
+print(f"Our shopping list is: {shopping_list}")
+print(f"An example with a dictionary with the information we have: {person_dict}")
+print(f"A tuple of colors to exemplify its usage: {color_tuple}")
+print(f"An example of a set of numbers: {set_numbers}")
